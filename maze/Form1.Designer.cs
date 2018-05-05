a927e1d414e65fda44e96de4126c0f7a7c12dce8
Cyril <born2445@gmail.com>
2018-05-05 22:42:46 +0300

    Changed design. Added logig. Debuging logic

--- maze/Form1.Designer.cs
+++ maze/Form1.Designer.cs
@@ -28,22 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.mouse = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mouse)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(550, 550);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(254, 577);
+            this.button1.Location = new System.Drawing.Point(131, 342);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -51,24 +50,79 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // mouse
+            // 
+            this.mouse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mouse.BackColor = System.Drawing.Color.Red;
+            this.mouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.mouse.Location = new System.Drawing.Point(66, 305);
+            this.mouse.Margin = new System.Windows.Forms.Padding(7);
+            this.mouse.Name = "mouse";
+            this.mouse.Size = new System.Drawing.Size(7, 7);
+            this.mouse.TabIndex = 2;
+            this.mouse.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 322);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(353, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(347, 323);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 612);
+            this.ClientSize = new System.Drawing.Size(712, 370);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.mouse);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mouse)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox mouse;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- maze/Form1.cs
+++ maze/Form1.cs
@@ -15,20 +15,252 @@ namespace maze
         public Form1()
         {
             InitializeComponent();
-            Bitmap start = new Bitmap(550, 550);
+            start = new Bitmap(pictureBox1.Image);
             for (int i = 0; i < start.Size.Width; i++)
             {
-                for (int j = 0; j < start.Size.Height; j++)
+                for (int j = 0; j < start.Height; j++)
                 {
-                    start.SetPixel(i, j, Color.Black);
+                    if (start.GetPixel(i, j).G == 0)
+                        start.SetPixel(i, j, Color.Black);
                 }
             }
+            movingUp = true;
+            movingDown = false;
+            movingLeft = false;
+            movingRight = false;
             pictureBox1.Image = start;
         }
-
+        Bitmap start;
+        bool movingUp, movingDown, movingLeft, movingRight;
         private void button1_Click(object sender, EventArgs e)
         {
+            timer1.Start();
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (wallBefore())
+            {
+                richTextBox1.Text += "\nСТЕНА ВПЕРЕДИ - ПОВОРАЧИВАЮ НАПРАВО - ИДУ ВПЕРЕД\n";
+                turnRight();
+                moveForward();
+                if (isExit())
+                {
+                    //УсПЕХ
+                    pictureBox1.BackColor = Color.Green;
+                    pictureBox1.Size = new Size(50, 50);
+                }
+                else if (wallBefore())
+                {
+                    richTextBox1.Text += "\nСТЕНА ВПЕРЕДИ - ИДУ НАЗАД - ПОВОРАЧИВАЮ НАЛЕВО - ИДУ ВПЕРЕД\n";
+                    moveBack();
+                    turnLeft();
+                    moveForward();
+                    if (wallBefore())
+                    {
+                        richTextBox1.Text += "\nСТЕНА ВПЕРЕДИ - ИДУ НАЗАД - ПОВОРАЧИВАЮ НАЛЕВО\n";
+                        moveBack();
+                        turnLeft();
+                    }
+                }
+            }
+            else
+            {
+                richTextBox1.Text += "\n БЛЯ СТЕНЫ НЕТ - ИДУ ВПЕРЕД\n";
+                moveForward();
+            }
+                
+        }
+        /// <summary>
+        /// Поворот вправо
+        /// </summary>
+        private void turnRight()
+        {
+            if (movingUp)
+            {
+                movingUp = false;
+                movingDown = false;
+                movingLeft = false;
+                movingRight = true;
+            }
+            else
+            {
+                if (movingDown)
+                {
+                    movingUp = false;
+                    movingDown = false;
+                    movingLeft = true;
+                    movingRight = false;
+                }
+                else
+                {
+                    if (movingLeft)
+                    {
+                        movingUp = true;
+                        movingDown = false;
+                        movingLeft = false;
+                        movingRight = false;
+                    }
+                    else
+                    {
+                        if (movingRight)
+                        {
+                            movingUp = false;
+                            movingDown = true;
+                            movingLeft = false;
+                            movingRight = false;
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Поворот налево
+        /// </summary>
+        private void turnLeft()
+        {
+            if (movingUp)
+            {
+                movingUp = false;
+                movingDown = false;
+                movingLeft = true;
+                movingRight = false;
+            }
+            else
+            {
+                if (movingDown)
+                {
+                    movingUp = false;
+                    movingDown = false;
+                    movingLeft = false;
+                    movingRight = true;
+                }
+                else
+                {
+                    if (movingLeft)
+                    {
+                        movingUp = false;
+                        movingDown = true;
+                        movingLeft = false;
+                        movingRight = false;
+                    }
+                    else
+                    {
+                        if (movingRight)
+                        {
+                            movingUp = true;
+                            movingDown = false;
+                            movingLeft = false;
+                            movingRight = false;
+                        }
+                    }
+                }
+            }
+        }
+        private bool isExit()
+        {
+            if ((mouse.Location.X > 147 && mouse.Location.X < 157) && (mouse.Location.Y >= 0 && mouse.Location.Y < 10))
+                return true;
+            return false;
+        }
+        private bool rightPass()
+        {
+            if ((start.GetPixel(mouse.Location.X + 1, mouse.Location.Y + 1).R >= 0 && movingDown) ||
+                (start.GetPixel(mouse.Location.X + 1, mouse.Location.Y - 1).R >= 0 && movingUp) ||
+                (start.GetPixel(mouse.Location.X + 1, mouse.Location.Y - 1).R >= 0 && movingRight) ||
+                (start.GetPixel(mouse.Location.X - 1, mouse.Location.Y + 1).R >= 0 && movingUp))
+                return true;
+            return false;
+
+        }
+        /// <summary>
+        /// Препятствие впереди 
+        /// </summary>
+        /// <returns></returns>
+        private bool wallBefore()
+        {
+            if (start.GetPixel(mouse.Location.X, mouse.Location.Y - 2).G == 0 && movingUp)
+                return true;
+            if (start.GetPixel(mouse.Location.X, mouse.Location.Y + 2).G == 0 && movingDown)
+                return true;
+            if (start.GetPixel(mouse.Location.X - 2, mouse.Location.Y).G == 0 && movingLeft)
+                return true;
+            if (start.GetPixel(mouse.Location.X + 2, mouse.Location.Y).G == 0 && movingRight)
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// Шаг назад
+        /// </summary>
+        private void moveBack()
+        {
+            if (movingUp)
+                moveDown();
+            if (movingDown)
+                moveUp();
+            if (movingLeft)
+                moveRight();
+            if (movingRight)
+                moveLeft();
+        }
+        /// <summary>
+        /// Шаг вперед
+        /// </summary>
+        private void moveForward()
+        {
+            if (movingUp)
+                moveUp();
+            if (movingDown)
+                moveDown();
+            if (movingLeft)
+                moveLeft();
+            if (movingRight)
+                moveRight();
+        }
+        private void moveUp()
+        {
+            movingUp = true;
+            movingDown = false;
+            movingLeft = false;
+            movingRight = false;
+            mouse.Location = new Point(mouse.Location.X, mouse.Location.Y - 1);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void moveDown()
+        {
+            movingUp = false;
+            movingDown = true;
+            movingLeft = false;
+            movingRight = false;
+            mouse.Location = new Point(mouse.Location.X, mouse.Location.Y + 1);
+        }
+        private void moveLeft()
+        {
+            movingUp = false;
+            movingDown = false;
+            movingLeft = true;
+            movingRight = false;
+            mouse.Location = new Point(mouse.Location.X -1 , mouse.Location.Y);
+        }
+        private void moveRight()
+        {
+            movingUp = false;
+            movingDown = false;
+            movingLeft = false;
+            movingRight = true;
+            mouse.Location = new Point(mouse.Location.X + 1, mouse.Location.Y);
         }
     }
 }
